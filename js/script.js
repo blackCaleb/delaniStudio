7bcee3fc3bb9660010f4dd5a96f7af0417f20d26
blackCaleb <bblackcaleb@gmail.com>
2021-11-22 21:09:37 +0300

    Worked on what we do page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-    
+    $(".col1").click(function(){
+      $("#doo1").toggle();
+      $("#wee1").toggle();
+    });
+
+    $(".col2").click(function(){
+      $("#doo2").toggle();
+      $("#wee2").toggle();
+    });
+
+    $(".col3").click(function(){
+      $("#doo3").toggle();
+      $("#wee3").toggle();
+    });
+
+
   });
