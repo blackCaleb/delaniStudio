34e2f005fb2789bc9c9fd8a9b4614bc53e60e00a
blackCaleb <bblackcaleb@gmail.com>
2021-11-23 03:49:50 +0300

    polished js file and also responsibility

--- js/script.js
+++ js/script.js
@@ -1,18 +1,60 @@
 $(document).ready(function() {
-    $(".col1").click(function(){
-      $("#doo1").toggle();
-      $("#wee1").toggle();
+
+  // What we do section
+    $('.design').click(function(){
+      $('#text1').toggle();
+      $('#image1').toggle();
     });
 
-    $(".col2").click(function(){
-      $("#doo2").toggle();
-      $("#wee2").toggle();
+    $('.development').click(function(){
+      $('#text2').toggle();
+      $('#image2').toggle();
     });
 
-    $(".col3").click(function(){
-      $("#doo3").toggle();
-      $("#wee3").toggle();
+    $('.product').click(function(){
+      $('#text3').toggle();
+      $('#image3').toggle();
     });
 
+    // portfolio section
+    $('#col1').hover(function(){
+      $('#txt1').toggle()
+      
+  })
+  
+  $('#col2').hover(function(){
+      $('#txt2').toggle()
+      
+  })
+  
+  $('#col3').hover(function(){
+  $('#txt3').toggle()
+      
+  })
+  
+  $('#col4').hover(function(){
+      $('#txt4').toggle()
+      
+  })
+  
+  $('#col5').hover(function(){
+      $('#txt5').toggle()
+      
+  })
+  
+  $('#col6').hover(function(){
+      $('#txt6').toggle()
+      
+  })
+  
+  $('#col7').hover(function(){
+      $('#txt7').toggle()
+      
+  })
+  
+  $('#col8').hover(function(){
+      $('#txt8').toggle()
+      
+  });
 
   });
